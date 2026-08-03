45adde86be69acda339b8c490480c1cb76255bda
hauf974 <haufbeute@gmail.com>
2026-08-03 17:04:23 +0400

    lot0: init monorepo npm workspaces (shared, server, client)

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+
+export default tseslint.config(
+  { ignores: ['**/dist/**', '**/node_modules/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['client/**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+);
